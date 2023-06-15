26efd4d36c87365526f52559e49fcc794ba66ef4
yaemsaeng <yaemsaeng2001@gmail.com>
2023-06-15 15:56:26 +0700

    ีupdate

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from routes.food_routes import food_Router
-# import requests
-# from pydantic import BaseModel 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(food_Router)
 
