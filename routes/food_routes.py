333f369cbd1848008de0db74942cd4a7c28647af
yaemsaeng <yaemsaeng2001@gmail.com>
2023-06-15 16:44:04 +0700

    update foodrouts

--- routes/food_routes.py
+++ routes/food_routes.py
@@ -14,23 +14,23 @@ cred = credentials.Certificate("./firebase/food-5fd0f-firebase-adminsdk-j0le2-69
 firebase_admin.initialize_app(cred, {'storageBucket': 'food-5fd0f.appspot.com'})
 bucket = storage.bucket()
 
-@food_Router.get("/image_url/{image_name}", tags=["food"])
-def get_image_url(image_name: str):
-    blob = bucket.blob(image_name)
-    url = blob.generate_signed_url(
-        expiration=datetime.timedelta(days=7),  # ตั้งค่าเวลาหมดอายุของ URL
-    )
-    return url
-
-@food_Router.get("/search/{Title}" , tags=["search food"]) 
+@food_Router.get("/search/{Title}", tags=["reccomend food"])
 async def get_similar_foods(Title: str):
     regex = re.compile(f".*{Title}.*", re.IGNORECASE)
     similar_foods = foods_serializer(collection.find({"Title": regex}))
-    return similar_foods
+    image_names = [food["Image_Name"] + ".jpg" for food in similar_foods]
 
-@food_Router.get("/reccomend/{Title}", tags=["reccomend food"])
-async def get_similar_foods(Title: str):
-    regex = re.compile(f".*{Title}.*", re.IGNORECASE)
-    similar_foods = collection.find({"Title": regex})
-    formatted_foods = [{"Title": food["Title"], "Ingredients": food["Ingredients"],"Image_Name":food["Image_Name"]} for food in similar_foods]
-    return formatted_foods
+    urls = []
+    for image_name in image_names:
+        blob = bucket.blob(image_name)
+        url = blob.generate_signed_url(
+            expiration=datetime.timedelta(days=7),
+        )
+        urls.append(url)
+
+    # เปลี่ยนค่า "Image_Name" ใน similar_foods เป็นค่าจาก urls
+    for i, food in enumerate(similar_foods):
+        food["Image_Name"] = urls[i]
+
+    return similar_foods
+    
