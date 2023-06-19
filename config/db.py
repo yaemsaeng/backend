16331d5ea38298113df4e338bf0c4ae5920cf0a5
yaemsaeng <yaemsaeng2001@gmail.com>
2023-06-19 16:09:26 +0700

    update db

--- config/db.py
+++ config/db.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
 db_connection = MongoClient("mongodb+srv://boat:1234@cluster0.rmsa1et.mongodb.net/")
 db = db_connection.myDB
-collection = db["Food"]
+collection = db["test"]
